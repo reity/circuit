0aad0e466c27272c6c938b62afeec43d3b225c92
Andrei Lapets <a@lapets.io>
2017-12-04 00:28:11 -0500

    Initial implementation and tests.

--- circuit/circuit.py
+++ circuit/circuit.py
@@ -10,6 +10,7 @@
 ##
 
 import doctest
+import operator
 
 ###############################################################################
 ##
@@ -24,9 +25,39 @@ class CircuitError(Exception):
 class circuit():
     """
     Class for circuits.
+
+    >>> circuit('x')
+    x
+    >>> (circuit('x') + circuit('x'))(1, 2)
+    3
+    >>> (circuit('x') + circuit('x') * circuit('x'))(1, 2, 3)
+    7
     """
-    def __init__(self):
-        pass
+    def __init__(self, s = (lambda cs: 'x'), f = None, a = 1, cs = []):
+        sf = (lambda cs: s) if type(s) is str else s # Display string to function.
+        for (k,v) in {'s':sf, 'f':f, 'a':a, 'cs':cs}.items():
+            setattr(self, k, v)
+
+    def __add__(self, oth):
+        return circuit((lambda cs: '('+str(cs[0])+' + '+str(cs[1])+')'), operator.add, self.a + oth.a, [self, oth])
+
+    def __mul__(self, oth):
+        return circuit((lambda cs: '('+str(cs[0])+' * '+str(cs[1])+')'), operator.mul, self.a + oth.a, [self, oth])
+
+    def __call__(self, *args):
+        if self.a == 1:
+            return args[0]
+        (vs, i) = ([], 0)
+        for c in self.cs:
+            vs.append(c(*args[i : i + c.a]))
+            i += c.a
+        return self.f(*vs)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return self.s(self.cs)
 
 if __name__ == "__main__": 
     doctest.testmod()

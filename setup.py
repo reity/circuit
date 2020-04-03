87256a07075b3588540b479a94314c955ff1ef83
Andrei Lapets <a@lapets.io>
2020-04-03 16:53:03 -0400

    Fix typo in initialization file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="circuit",
-    version="0.1.0.0",
+    version="0.1.0.1",
     packages=["circuit",],
     install_requires=["parts",],
     license="MIT",

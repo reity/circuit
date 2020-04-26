87e8e43b5b131286586285f1356feeafcbd6053b
Andrei Lapets <a@lapets.io>
2020-04-25 23:28:18 -0400

    Update package version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="circuit",
-    version="0.1.0.4",
+    version="0.1.0.5",
     packages=["circuit",],
     install_requires=["parts",],
     license="MIT",

1c2fd01ae263edb3ee272bcda4f80e7d29fa6476
Andrei Lapets <a@lapets.io>
2020-09-28 20:09:26 -0400

    Update package version and license file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="circuit",
-    version="0.3.0",
+    version="0.3.1",
     packages=["circuit",],
     install_requires=[
         "parts>=0.2.1",
